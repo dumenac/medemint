d77d805a383e9f73c6f6ffe2899e8b30310170c1
Domenec <dmele@alumni.unav.es>
2023-05-23 17:55:07 -0500

    Set up environment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
+  runtimeConfig: {
+    // The private keys which are only available within server-side
+    // Keys within public, will be also exposed to the client-side
+    openaiSecret: "",
+    crossmintBaseUrl: "" || "https://staging.crossmint.com",
+    crossmintProjectId: "",
+    crossmintClientSecret: "",
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
---
+++ server/api/hello.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler(async (event) => {
+
+    const env = useRuntimeConfig()
+
+    
+
+})
