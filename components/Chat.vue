488c1620d63ebde1cf8317216dea1bc407c6fc21
Domenec <dmele@alumni.unav.es>
2023-05-23 23:02:04 -0500

    Polished the UI

--- components/Chat.vue
+++ components/Chat.vue
@@ -1,11 +1,100 @@
 <template>
   <div class="min-w-[80vw] xl:min-w-[40vw]">
-    <form action="" class="w-full space-y-4 flex flex-col">
+    <form
+      class="w-full space-y-4 flex flex-col"
+      @submit.prevent="mint"
+      v-if="!nft"
+    >
+      {{ data }}
       <textarea
+        v-model="prompt"
         placeholder="Please mint an NFT on the Polygon blockchain of a Monkey with blue eyes wearing a hawaiian shirt and send it to domenec@crossmint.io"
         class="p-2 min-h-[150px] rounded-lg border border-gray-200 w-full"
       />
-      <input type="submit" value="Mint my NFT" class="py-2 px-5 rounded-lg bg-green-600 text-white ml-auto">
+      <button
+        v-if="!loading"
+        type="submit"
+        value="Mint my NFT"
+        class="py-2 px-5 rounded-lg bg-green-600 text-white ml-auto"
+      >
+        Mint my NFT
+      </button>
+      <p v-else class="py-2 px-5 ml-auto">Loading...</p>
     </form>
+    <div class="space-y-4" v-else>
+      <div class="md:flex">
+        <img
+          :src="data.data.request.metadata.image"
+          alt=""
+          class="m-auto md:m-none h-96 rounded"
+        />
+        <div class="p-8 space-y-8">
+          <div class="space-y-2">
+            <h1 class="text-2xl font-semibold">
+              {{ data.data.request.metadata.name }}
+            </h1>
+            <h2 class="text-base font-light">
+              {{ data.data.request.metadata.description }}
+            </h2>
+          </div>
+          <div class="grid grid-cols-2 lg:grid-cols-3 gap-4">
+            <div
+              v-for="attribute in data.data.request.metadata.attributes"
+              class="p-4 border rounded-lg space-y-2 text-center"
+            >
+              <h1 class="uppercase text-sm">{{ attribute.value }}</h1>
+              <h1 class="uppercase text-sm font-semibold">
+                {{ attribute.trait_type.replace("_", " ") }}
+              </h1>
+            </div>
+          </div>
+          <p class="font-light">
+            This NFT is living on the
+            <span class="capitalize">{{
+              data.data.response.onChain.chain
+            }}</span>
+            blockchain.
+            <span v-if="data.data.response.onChain.chain == 'polygon'"
+              >Its contract address is
+              {{ data.data.response.onChain.contractAddress }}.</span
+            >
+          </p>
+        </div>
+      </div>
+      <button class="py-2 px-4 border rounded bg-green-500 text-white" @click="refresh">Mint another NFT</button>
+    </div>
   </div>
 </template>
+
+<script setup>
+const loading = ref(false);
+const nft = ref(false);
+const prompt = ref("");
+const data = ref("");
+
+const mint = async () => {
+  try {
+    loading.value = true;
+    const post = await useAsyncData(() =>
+      $fetch(`/api/run`, {
+        body: {
+          description: prompt.value,
+        },
+        method: "POST",
+      })
+    );
+    data.value = post;
+  } catch (e) {
+    message.value = e.description || e.message;
+  } finally {
+    loading.value = false;
+    nft.value = true;
+  }
+};
+
+const refresh = async () => {
+  nft.value = false;
+  loading.value = false;
+  data.value = ''
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="m-auto">
-        <Chat/>
+        <Chat v-if="!submitted"/>
     </div>
 </template>
+
+<script setup>
+
+const submitted = ref(false)
+
+</script>
